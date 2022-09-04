c2641d2d14743914b4563ad44bada1baa7c952c4
Joe-x007 <101728021+Joe-x007@users.noreply.github.com>
2022-09-04 20:40:25 +0200

    Reset password
    functionality for reset password added

---
+++ src/ResetPassword.js
@@ -0,0 +1,39 @@
+import {useState} from 'react'
+import './forms.css'
+import { Link } from 'react-router-dom'
+import { getAuth , sendPasswordResetEmail} from 'firebase/auth'
+
+
+function ResetPassword() {
+
+  const [email, setEmail] = useState('')
+  const auth = getAuth();
+
+  const triggerResetEmail = async () => {
+    await sendPasswordResetEmail(auth, email);
+    console.log("Password reset email sent")
+  }
+ 
+  return (
+    <div className='center'>
+    <div className='auth'>
+      <h1>Reset Password</h1>
+      <form onSubmit={ResetPassword} name='login_form'>
+        <input 
+          type='email' 
+          value={email}
+          required
+          placeholder="Enter your email"
+          onChange={e => setEmail(e.target.value)}/>
+        <button className="resetBtn" type="button" onClick={triggerResetEmail}>Reset password</button>
+      </form>
+      <p>
+           Oops i want to go back! 
+          <Link to='/login'>Click here</Link>
+        </p>
+    </div>
+  </div>
+  )
+}
+
+export default ResetPassword;
