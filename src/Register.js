df22bc18ba097816c5a97937c794a21df6462781
mzaidally <85238276+mzaidally@users.noreply.github.com>
2022-09-07 00:05:50 +0200

    Add files via upload

---
+++ src/Register.js
@@ -0,0 +1,126 @@
+import {useState} from 'react'
+
+import './forms.css'
+import {app, auth} from './firebase'
+import {Link} from 'react-router-dom'
+import {createUserWithEmailAndPassword} from 'firebase/auth'
+
+import {addDoc, collection, getFirestore} from "firebase/firestore";
+
+import {useHistory} from 'react-router-dom'
+function Register() {
+
+  //instantiating variables
+  const [email, setEmail] = useState('')
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+  const [error, setError] = useState('')
+  const history = useHistory()
+
+  const db = getFirestore(app);
+
+  //add user details to firebase realtime databse
+  function addToUsersTable(username, email) {
+
+    try {
+      const docRef =  addDoc(collection(db, "users"), {
+        username: username,
+        email: email,
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+
+  }
+
+  // validate password so 'password' and 'confirm password' match
+  const validatePassword = () => {
+    let isValid = true
+    //if both fields are not empty
+    if (password !== '' && confirmPassword !== ''){
+      //if passwords do not match
+      if (password !== confirmPassword) {
+        isValid = false
+        setError('Passwords does not match')
+      }
+    }
+    return isValid
+  }
+
+  const register = e => {
+    e.preventDefault()
+    setError('')
+
+    //password is validated successfully
+    if(validatePassword()) {
+      // Create a new user with email and password using firebase
+        createUserWithEmailAndPassword(auth, email, password)
+            //verification screen is displayed
+        .then((res) => {
+          
+          
+        auth.signOut();
+        alert("You've registered!");
+        history.push('/Login')
+          
+          })
+        .catch(err => setError(err.message))
+          
+      //add user to user table
+      addToUsersTable(username, email)
+
+    }
+    setEmail('')
+    setPassword('')
+    setConfirmPassword('')
+  }
+
+  return (
+    <div className='center'>
+      <div className='auth'>
+        <h1>Register</h1>
+        {error && <div className='auth__error'>{error}</div>}
+        <form onSubmit={register} name='registration_form'>
+          <input 
+            type='email' 
+            value={email}
+            placeholder="Enter your email"
+            required
+            onChange={e => setEmail(e.target.value)}/>
+
+          <input
+              type='username'
+              value={username}
+              required
+              placeholder='Enter a new username'
+              onChange={e => setUsername(e.target.value)}
+              />
+
+          <input 
+            type='password'
+            value={password} 
+            required
+            placeholder='Enter your password'
+            onChange={e => setPassword(e.target.value)}/>
+
+            <input 
+            type='password'
+            value={confirmPassword} 
+            required
+            placeholder='Confirm password'
+            onChange={e => setConfirmPassword(e.target.value)}/>
+
+          <button type='submit'>Register</button>
+        </form>
+        <span>
+          Already have an account?  
+          <Link to='/login'>login</Link>
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default Register
