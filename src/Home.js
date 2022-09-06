33929bd0ebf4f69438e0dbca9f9878b1020ab1ba
Fatima-Essa <93796656+Fatima-Essa@users.noreply.github.com>
2022-09-06 20:39:25 +0200

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Switch} from 'react-router-dom'
 import Profile from './pages/Profile'
 
 import {AuthProvider} from './AuthContext'
